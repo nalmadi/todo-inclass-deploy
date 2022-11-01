2213ba0aa9102dab7f860ad7f9039d327257ddd6
nalmadi <nsalmadi@colby.edu>
2022-11-01 11:40:36 -0400

    fixed environ line for db

--- website/__init__.py
+++ website/__init__.py
@@ -15,7 +15,7 @@ def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.envron.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
 
     from .models import Note, User
